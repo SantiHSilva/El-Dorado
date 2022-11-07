9f0ab26d16fb435cffb0fd48fd3b80d5e756844e
YosstinCode <yosstincode@gmail.com>
2022-11-06 22:11:27 -0600

    fixed modal

--- gestion/views.py
+++ gestion/views.py
@@ -21,15 +21,18 @@ class FormularioInformacionView(HttpRequest):
 
     def modificar_producto(request, id):
         producto = get_object_or_404(Informacion, id=id)
+        print('producto:',producto.cantidad_productos)
         data = {
             'form': formularioInformacion(instance=producto)
         }
+        print('data:',data['form'])
         if request.method == 'POST':
             formulario = formularioInformacion(data=request.POST, instance=producto, files = request.FILES)
+            print("formulario:", formulario)
             if formulario.is_valid():
-                formulario.save()
+                producto.update(**formulario.cleaned_data)
                 producto.save()
-                return redirect(to='index')
+                return redirect(to='/')
             data["form"] = formulario
         return render(request, 'modificar_producto.html', data)
                 
