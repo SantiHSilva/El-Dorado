cda83996085bc6db7f2c6c8576a0242f154b58f2
Carrotw <91509157+MetTheCarrot@users.noreply.github.com>
2022-11-08 04:41:22 -0500

    ¿?
    Co-Authored-By: YosstinCode <76113738+YosstinCode@users.noreply.github.com>

--- gestion/templatetags/filter.py
+++ gestion/templatetags/filter.py
@@ -37,19 +37,14 @@ def transform_date(fecha):
 
 @register.simple_tag
 def conversion_g_a_kg(value, unidades, *args, **kwargs):
-    print(f'Peso: {value}')
-    print(f'Unidades: {unidades}')
     if unidades == "2":
-        print("Transformado a kg")
         return (f"{round(value / 1000,2)}kg")
     else:
-        print("No se transforma")
         return value
 
 
 @register.filter(name='transform_unidades')
 def transform_unidades(unidades):
-    print(type(unidades))
     if unidades == "1":
         return "Kilogramos"
     elif unidades == "2":
